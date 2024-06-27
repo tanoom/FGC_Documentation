79c7b3a7e48ec7aad03e06e60e05f63d8b9fc6d4
tanoom <68962425+tanoom@users.noreply.github.com>
2024-06-26 20:40:24 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ release = '0.0.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx_rtd_theme',
+    'sphinx_rtd_theme'
 ]
 
 templates_path = ['_templates']
